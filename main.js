08744b6110d6b5682849cd6f125b6cad33ede807
SangMin-Code <ragdexplorer@gmail.com>
2021-08-25 16:16:33 +0900

    nav backgroudn color edit
    background-color opacity를 scroll위치에 따라 값을 설정하던 방법에서  transition으로 바꿈.

--- main.js
+++ main.js
@@ -4,17 +4,12 @@ const body = document.body;
 const navBar = document.querySelector('#navbar');
 const navBarRect = navBar.getBoundingClientRect();
 
-window.addEventListener('scroll', (e) => {
-	navBar.style.backgroundColor = `rgba(141, 191, 248, ${
-		window.scrollY / 1000
-	})`;
-
-	if (
-		window.scrollY > navBarRect.height &&
-		!body.classList.contains('scroll')
-	) {
+document.addEventListener('scroll', (e) => {
+	if (window.scrollY > navBarRect.height) {
 		body.classList.add('scroll');
-	} else if (window.scrollY <= 200) {
+		navBar.classList.add('onscroll');
+	} else {
 		body.classList.remove('scroll');
+		navBar.classList.remove('onscroll');
 	}
 });
