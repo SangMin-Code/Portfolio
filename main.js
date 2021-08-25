11e0c9c501c444f654901531dc5fc2bef70df268
SangMin-Code <ragdexplorer@gmail.com>
2021-08-25 16:45:19 +0900

    js nav click scroll to section
    add click event that scroll to section to nav menu and contact btn

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// transparent NavBar when it on the top
 const body = document.body;
 const navBar = document.querySelector('#navbar');
 const navBarRect = navBar.getBoundingClientRect();
@@ -13,3 +14,32 @@ document.addEventListener('scroll', (e) => {
 		navBar.classList.remove('onscroll');
 	}
 });
+
+// navbar scrollTo
+const navbarMenu = document.querySelector('ul.navbar__menu');
+navbarMenu.addEventListener('click', (e) => {
+	const target = e.target;
+	if (target.classList.contains('navbar__menu__item')) {
+		removeActive('navbar__menu__item');
+		target.classList.add('active');
+		const toSection = target.dataset.section;
+		const section = document.querySelector(`#${toSection}`);
+		section.scrollIntoView({ behavior: 'smooth' });
+	}
+});
+
+//contactme scrollTo
+const contactBtn = document.querySelector('.home_btn');
+contactBtn.addEventListener('click', (e) => {
+	const contact = document.querySelector('#contact');
+	contact.scrollIntoView({ behavior: 'smooth' });
+});
+
+function removeActive(className) {
+	const elements = document.querySelectorAll(`.${className}`);
+	elements.forEach((element) => {
+		if (element.classList.contains('active')) {
+			element.classList.remove('active');
+		}
+	});
+}
